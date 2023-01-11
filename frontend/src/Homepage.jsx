23af0feab0deab7780f22c0337af95e3d98823c4
Andrew Brongar <andrewbrongar@gmail.com>
2023-01-11 00:31:48 -0300

    front up
    adicionado logo
    botões personalizados, mas não finalizados.

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 
-export const Homepage = (props) => { 
+export const Homepage = (props) => {
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState ('');
+    const [password, setPassword] = useState('');
     let contentArray = [""];
     const [content, setContent] = useState("");
     const [inContent, setinContent] = useState(false);
@@ -10,15 +10,15 @@ export const Homepage = (props) => {
     async function getData(url = '') {
         console.log("fetching...");
         const response = await fetch(url, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json'
-          }
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
         });
         return response.json();
-      }
+    }
 
-    async function handleSubmit(){
+    async function handleSubmit() {
         console.log(inContent);
         if (!(inContent)) {
             console.log("Function called!");
@@ -27,15 +27,21 @@ export const Homepage = (props) => {
                     console.log("Data received!");
                     for (const comm in data) {
                         let currentPosts = eval(data[comm].posts);
-                        let prop1 = {title: currentPosts[0].postTitle, author: currentPosts[0].postAuthor,
-                                     date: currentPosts[0].postData.toUTCString(), description: currentPosts[0].postDesc,
-                                     karma: currentPosts[0].postKarma, community: data[comm].nameCommunity}
-                        let prop2 = {title: currentPosts[1].postTitle, author: currentPosts[1].postAuthor,
-                                     date: currentPosts[1].postData.toUTCString(), description: currentPosts[1].postDesc,
-                                     karma: currentPosts[1].postKarma, community: data[comm].nameCommunity}
-                        let prop3 = {title: currentPosts[2].postTitle, author: currentPosts[2].postAuthor,
-                                     date: currentPosts[2].postData.toUTCString(), description: currentPosts[2].postDesc,
-                                     karma: currentPosts[2].postKarma, community: data[comm].nameCommunity}
+                        let prop1 = {
+                            title: currentPosts[0].postTitle, author: currentPosts[0].postAuthor,
+                            date: currentPosts[0].postData.toUTCString(), description: currentPosts[0].postDesc,
+                            karma: currentPosts[0].postKarma, community: data[comm].nameCommunity
+                        }
+                        let prop2 = {
+                            title: currentPosts[1].postTitle, author: currentPosts[1].postAuthor,
+                            date: currentPosts[1].postData.toUTCString(), description: currentPosts[1].postDesc,
+                            karma: currentPosts[1].postKarma, community: data[comm].nameCommunity
+                        }
+                        let prop3 = {
+                            title: currentPosts[2].postTitle, author: currentPosts[2].postAuthor,
+                            date: currentPosts[2].postData.toUTCString(), description: currentPosts[2].postDesc,
+                            karma: currentPosts[2].postKarma, community: data[comm].nameCommunity
+                        }
                         contentArray.push(childrenPost(prop1));
                         contentArray.push(childrenPost(prop2));
                         contentArray.push(childrenPost(prop3));
@@ -43,29 +49,31 @@ export const Homepage = (props) => {
                     setContent(contentArray);
                     setinContent(true);
                 });
-        }    
+        }
     }
 
-    function childrenPost(props){
+    function childrenPost(props) {
         return (
             <div className="homepage-post">
+                <div style={{ backgroundImage: 'url(backgroundHome.png)' }}></div>
                 <h3> {props.community} </h3>
                 <h2> {props.title} por {props.author} em {props.date} </h2>
-                <p> {props.description} </p> 
-                <p className="homepage-post_karma">  {props.karma} </p> 
+                <p> {props.description} </p>
+                <p className="homepage-post_karma">  {props.karma} </p>
             </div>
         );
     }
 
-    useEffect(() => {handleSubmit();});
+    useEffect(() => { handleSubmit(); });
 
     return (
-        <div className = "homepage-container">
+        <div className="homepage-container">
             <h2>Homepage</h2>
-            <div className = "homepage-container_post-history">
+            <button className="link-btn" onClick={() => props.onFormSwitch('login')}> Login </button>
+            <div className="homepage-container_post-history">
                 {content}
             </div>
-            <button className = "link-btn" onClick = {() => props.onFormSwitch('login')}> Login! </button>
+
         </div>
     )
 } 
--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,40 +1,46 @@
 import React, { useState } from "react";
+import logo from './headit.png';
 
-export const Login = (props) => { 
+export const Login = (props) => {
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState ('');
+    const [password, setPassword] = useState('');
 
     async function postData(url = '', data = {}) {
         const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
         });
         return response.json();
-      }
+    }
 
-const handleSubmit = (ev) => {
-    ev.preventDefault();
-    postData('HTTP://localhost:3001/auth', { email: email, password: password })
-        .then((data) => {
-            console.log(data);
-        });        
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        postData('HTTP://localhost:3001/auth', { email: email, password: password })
+            .then((data) => {
+                console.log(data);
+            });
 
-}
+    }
 
     return (
-        <div className = "auth-form-container">
-            <h2>Login</h2>
-            <form className = "login-form" onSubmit = {handleSubmit} >
-                <label htmlFor = "email"> Email </label>
-                <input value = {email} onChange={(ev) => setEmail(ev.target.value)} type = "email" placeholder = "Digite seu email" id = "email" name = "email" /> 
-                <label htmlFor = "password"> Senha </label>
-                <input value = {password} onChange={(ev) => setPassword(ev.target.value)} type = "password" placeholder = "Digite sua senha" id = "password" name = "password" /> 
-                <button type = "submit" >Login</button>
+        <div className="auth-form-container">
+            <div className="logo">
+                <img src={logo} alt="logo" width="100" height="100" margin="0 50px" margin-left="auto" margin-right="auto" />
+            </div>
+
+            <form className="login-form" onSubmit={handleSubmit} >
+                <label htmlFor="email"> Email </label>
+                <input value={email} onChange={(ev) => setEmail(ev.target.value)} type="email" placeholder="Digite seu email" id="email" name="email" />
+                <label htmlFor="password"> Senha </label>
+                <input value={password} onChange={(ev) => setPassword(ev.target.value)} type="password" placeholder="Digite sua senha" id="password" name="password" />
+                <div class="button">
+                    <button type="submit" >Login</button>
+                </div>
             </form>
-            <button className = "link-btn" onClick = {() => props.onFormSwitch('register')}> Deseja se cadastrar? Clique aqui. </button>
+            <button className="cad" onClick={() => props.onFormSwitch('register')}> Deseja se cadastrar? Clique aqui. </button>
         </div>
     )
 } 
